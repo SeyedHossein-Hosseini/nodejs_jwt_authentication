3016e8f55bfd85c8fc88f39869b33f1f07722142
Hossein-Joon <syd.hosseini.1998@gmail.com>
2022-11-06 23:18:18 +0330

    Error handling and jwt in ui implemented

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,26 @@
 const User = require("../models/User");
 
+const jwt = require('jsonwebtoken');
+
 // based on seconds
 const maxAge = 100;
 
+
+// let jwtSecretKey = process.env.JWT_SECRET_KEY;
+// let data = {
+//   time: Date(),
+//   userId: 12,
+// }
+
+// const token = jwt.sign(data, jwtSecretKey);
+
+// res.send(token);
+
+
+const createToken = (id) => {
+  let data = { time: Date(), userId: id };
+  return jwt.sign(data, process.env.JWT_SECRET_STRING);
+};
 
 const handleErrors = (errors) => {
   let _errors = { email: '', password: '' };
@@ -26,13 +44,14 @@ module.exports.signup_get = (req, res) => {
 
 module.exports.signup_post = async (req, res) => {
   const { email, password } = req.body;
-  console.log(req.body);
   try {
     const user = await User.create({ email, password });
+    const token = createToken(user._id);
+    res.cookie('jwt', token);
     res.status(201).json(user._id);
   } catch (err) {
     const errors = handleErrors(err);
-    res.status(400).json(errors);
+    res.status(400).json({errors});
   }
 };
 
